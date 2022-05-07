d742db9bcdecb489c6a82e2f1a10dd48b4c14c32
Mar43nk0 <markwalikdj@gmail.com>
2022-05-07 20:37:50 +0300

    location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,47 @@ import { LocationResponse, WeatherResponse } from './weatherDTOs';
 export class AppComponent {
 
   constructor(private weatherService: WeatherService) {
-
+    this.getUserLocation();
   }
 
+  isFinished: boolean = false;
   inputCity: string;
   location: LocationResponse;
   weather: WeatherResponse;
+  imagePath: string;
 
-  title = 'WeatherApp';
+
+  getUserLocation() {
+    if(!navigator.geolocation) return;
+    navigator.geolocation.getCurrentPosition(async position => {
+      await this.getLocationByPosition(position);
+      await this.updateByLocation();
+    });
+  }
+
+  async getLocationByPosition(position: GeolocationPosition) {
+    let lat = position.coords.latitude;
+    let lng = position.coords.longitude;
+    let locations = await this.weatherService.getLocationBy(lat, lng);
+    this.location = locations[0];
+  }
 
   async onSearch() {
     let locations = await this.weatherService.getLocation(this.inputCity);
     this.location = locations[0];
+
+    await this.updateByLocation();
+  }
+
+  async updateByLocation() {
     this.weather = await this.weatherService.getForecast(this.location.woeid);
-    console.log('this.weather', this.weather);
+
+    this.updateImagePath();
+    this.isFinished = true;
+  }
+
+  updateImagePath() {
+    this.imagePath = "https://www.metaweather.com/static/img/weather/" + this.weather.consolidated_weather[0].weather_state_abbr + ".svg";
+    console.log('this.imagePath', this.imagePath);
   }
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { LocationResponse, WeatherResponse } from './weatherDTOs';
 
@@ -14,6 +14,7 @@ export class WeatherService {
 
   constructor(private http: HttpClient) { }
 
+  //локация по маске
   getLocation(location: string): Promise<LocationResponse> {
     return this.http
           .get<LocationResponse>(`${baseUrl}/location/search/?query=${location}`)
@@ -25,6 +26,19 @@ export class WeatherService {
           ).toPromise();
   }
 
+  //локация по маске
+  getLocationBy(lat: number, lng: number): Promise<LocationResponse> {
+    return this.http
+          .get<LocationResponse>(`${baseUrl}/location/search/?lattlong=${lat},${lng}`)
+          .pipe(
+              catchError((error: HttpErrorResponse) => {
+                  alert(error.message + ' status: ' + error.status + ' text ' + error.statusText);
+                  return throwError(error.message);
+              })
+          ).toPromise();
+  }
+
+  //прогноз по айди локации
   getForecast(woeid: number): Promise<WeatherResponse> {
     return this.http
           .get<WeatherResponse>(`${baseUrl}/location/${woeid}`)
